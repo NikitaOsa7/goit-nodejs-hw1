0841244d474207e9a029ea87c6b764911bd0e5de
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-08-15 12:22:40 +0300

    ю

--- contacts.js
+++ contacts.js
@@ -4,31 +4,32 @@ const crypto = require('crypto');
 const contactsPath = './db/contacts.json';
 
 module.exports = {
-    listContacts: async function () {
-        const contacts = JSON.parse(await fs.readFile(contactsPath));
-        return contacts;
-    },
-    getContactsById: async function (contactId) {
-        const contacts = await this.listContacts();
-        const contact = contacts.find(({ id }) => id === contactId.toString());
-        if (!contact) return `Don't find contact with contactId = ${contactId} `;
-        return console.log(`Contact:, contact`);
-    },
+  listContacts: async function () {
+    const contacts = JSON.parse(await fs.readFile(contactsPath));
+    return contacts;
+  },
 
-    addContact: async function ({ name, email, phone }) {
-        const contacts = await this.listContacts();
-        const id = crypto.randomUUID();
-        const updatedContacts = [...contacts, { id, name, email, phone }];
-        await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
-        return console.log('Success');
-    },
+  getContactById: async function (contactId) {
+    const contacts = await this.listContacts();
+    const contact = contacts.find(({ id }) => id === contactId.toString());
+    if (!contact) return `Don't find contact with contactId = ${contactId} `;
+    return console.log(`Contact:`, contact);
+  },
 
-    removeContact: async function (contactId) {
-        const contacts = await this.listContacts();
-        const updatedContacts = contacts.filter(
-            ({ id }) => id !== contactId.toString()
-        );
-        await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
-        return console.log(`Deleting success`);
-    },
+  addContact: async function ({ name, email, phone }) {
+    const contacts = await this.listContacts();
+    const id = crypto.randomUUID();
+    const updatedContacts = [...contacts, { id, name, email, phone }];
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
+    return console.log('Success');
+  },
+
+  removeContact: async function (contactId) {
+    const contacts = await this.listContacts();
+    const updatedContacts = contacts.filter(
+      ({ id }) => id !== contactId.toString()
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
+    return console.log('Success');
+  },
 };
