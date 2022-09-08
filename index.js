81c13c2d1d3f18edfd22c51ead45e698f747c752
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-09-08 14:52:36 +0300

    done

--- index.js
+++ index.js
@@ -3,40 +3,43 @@ const contacts = require('./contacts');
 const { Command } = require('commander');
 const program = new Command();
 program
-    .option('-a, --action <type>', 'choose action')
-    .option('-i, --id <type>', 'user id')
-    .option('-n, --name <type>', 'user name')
-    .option('-e, --email <type>', 'user email')
-    .option('-p, --phone <type>', 'user phone');
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
-program.parse(precess.argv);
+program.parse(process.argv);
 
 const argv = program.opts();
 
-async function invokeAction({ action, id, email, name, phone }) {
-    switch (action) {
-        case 'list':
-            const contactsList = await contacts.listContacts();
-            return console.table(contactsList);
-        case 'get':
-            return await contacts.getContactsById(id);
-        case 'add':
-            return contacts.addContact({ name, email, phone });
-        case 'remove':
-            return contacts.removeContact(id);
-        
-        default: 
-            console.warn('Unknown action type!')
-    }
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const contactsList = await contacts.listContacts();
+      return console.table(contactsList);
+
+    case 'get':
+      return await contacts.getContactById(id);
+
+    case 'add':
+      return contacts.addContact({ name, email, phone });
+
+    case 'remove':
+      return contacts.removeContact(id);
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
 }
 
+// invokeAction(argv);
+
 const start = async argv => {
-    try {
-        await invokeAction(argv);
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    await invokeAction(argv);
+  } catch (err) {
+    console.log(err.message);
+  }
 };
-
-
 start(argv);
